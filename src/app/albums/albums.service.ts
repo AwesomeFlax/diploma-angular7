cdb2b8183b0490368d2c940771bd2bc00775e099
Denis Krutko <artstyleflax@gmail.com>
2019-01-13 18:28:17 +0100

    few updates, artists view

--- src/app/albums/albums.service.ts
+++ src/app/albums/albums.service.ts
@@ -7,12 +7,12 @@ export class AlbumsService {
     selectedAlbum = new EventEmitter<Album>();
 
     albums: Album[] = [
-        new Album(0, "name0", "genre0", "2014-9-15", "desc0", "cover0", [ new Track (1, 'track_0_1'), new Track (1, 'track_0_2') ] ),
-        new Album(1, "name1", "genre1", "2015-9-15", "desc1", "cover1", [ new Track (1, 'track_1_1'), new Track (1, 'track_1_2') ] ),
-        new Album(2, "name2", "genre2", "2016-9-15", "desc2", "cover2", [ new Track (1, 'track_2_1'), new Track (1, 'track_2_2') ] ),
-        new Album(3, "name3", "genre3", "2017-9-15", "desc3", "cover3", [ new Track (1, 'track_3_1'), new Track (1, 'track_3_2') ] ),
-        new Album(4, "name4", "genre4", "2018-9-15", "desc4", "cover4", [ new Track (1, 'track_4_1'), new Track (1, 'track_4_2') ] ),
-        new Album(5, "name5", "genre5", "2019-9-15", "desc5", "cover5", [ new Track (1, 'track_5_1'), new Track (1, 'track_5_2') ] ),
+        new Album(0, "Mansion", "Hip-Hop", "2014-9-15", "desc0", "https://ecsmedia.pl/c/mansion-w-iext46383085.jpg", [ new Track (1, 'Outcast'), new Track (1, 'Destiny') ] ),
+        new Album(1, "Perception", "Hip-Hop", "2015-9-15", "desc1", "https://i2.cdn.hhv.de/catalog/475x475/00565/565327.jpg", [ new Track (1, 'Notepad'), new Track (1, 'Trun The Music Up') ] ),
+        new Album(2, "Kamikaze", "Rap", "2016-9-15", "desc2", "https://upload.wikimedia.org/wikipedia/ru/thumb/1/1c/Eminem-kamikaze.jpg/270px-Eminem-kamikaze.jpg", [ new Track (1, 'Venom'), new Track (1, 'Stepping Stone') ] ),
+        new Album(3, "Revival", "Rap", "2017-9-15", "desc3", "https://upload.wikimedia.org/wikipedia/ru/thumb/6/6f/Eminem_Revival.jpg/270px-Eminem_Revival.jpg", [ new Track (1, 'Walk On Water'), new Track (1, 'Bad Husband') ] ),
+        new Album(4, "25", "Soul", "2018-9-15", "desc4", "https://upload.wikimedia.org/wikipedia/ru/thumb/a/aa/Adele_25.jpg/270px-Adele_25.jpg", [ new Track (1, 'Hello'), new Track (1, 'Send My Love') ] ),
+        new Album(5, "21", "Soul", "2019-9-15", "desc5", "https://images.store.hmv.com/app_/responsive/HMVStore/media/product/763088/01-763088.jpg?w=500", [ new Track (1, 'Rolling In The Deep'), new Track (1, 'Set Fire To The Rain') ] ),
     ]
 
     getAlbums(): Album[]
--- src/app/artists/artitst.service.ts
+++ src/app/artists/artitst.service.ts
@@ -12,6 +12,12 @@ export class ArtistsService {
         new Artist(2, 'NF', 'Nathan', 'John Feuerstein', new Date('09-15-1956'), null, new Date('09-15-1997'), null, 'USA', 
         'Male', 'same crap', 'https://i1.sndcdn.com/avatars-000333695771-obix8y-t500x500.jpg', null),
         new Artist(3, 'Adele', 'Adele Laurie', 'Blue Adkins', new Date('09-15-1986'), null, new Date('09-15-1997'), null, 'Great Britain', 
+        'Female', 'easy breazy', 'https://imgcache.qq.com/music/photo/mid_singer_500/p/x/003CoxJh1zFPpx.jpg', null),
+        new Artist(4, 'Eminem', 'Mashal', 'Matters', new Date('09-15-1987'), null, new Date('09-15-1997'), null, 'USA', 
+        'Male', 'kinda complex', 'https://img2.thejournal.ie/inline/4378295/original/?width=500&version=4378295', null),
+        new Artist(5, 'NF', 'Nathan', 'John Feuerstein', new Date('09-15-1956'), null, new Date('09-15-1997'), null, 'USA', 
+        'Male', 'same crap', 'https://i1.sndcdn.com/avatars-000333695771-obix8y-t500x500.jpg', null),
+        new Artist(6, 'Adele', 'Adele Laurie', 'Blue Adkins', new Date('09-15-1986'), null, new Date('09-15-1997'), null, 'Great Britain', 
         'Female', 'easy breazy', 'https://imgcache.qq.com/music/photo/mid_singer_500/p/x/003CoxJh1zFPpx.jpg', null)
     ];
 
